508d92ac989c38495e2a0f904bc5f86e3f0b7949
arifurrahmansajid <iamnobody3456@gmail.com>
2024-11-30 18:48:40 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Nova Tourism Server
+
+## Description
+
+This is the server-side code for the Nova Tourism application. It is built with Node.js, Express, and MongoDB.
+
+**Visit the live site:** [Nova Travel](https://nova-tourism.web.app/).
+
+[Client side repository](https://github.com/HunterMahmud/nova-tourism-client)
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- **Node.js**: Install Node.js (version 14 or higher) from [nodejs.org](https://nodejs.org/).
+- **npm**: Node.js package manager (npm) should be installed along with Node.js.
+- **MongoDB**: Install MongoDB from [mongodb.com](https://www.mongodb.com/).
+
+## Installation
+
+Follow these steps to set up your development environment:
+
+1. **Clone the repository**:
+
+   ```sh
+   git clone https://github.com/HunterMahmud/nova-tourism-server.git
+   cd nova-tourism-server
+   ```
+
+2. **Install dependencies**:
+
+   ```sh
+   npm install
+   ```
+
+3. **Set up environment variables**:
+
+   Create a `.env` file in the root of the project and add the necessary environment variables. For example:
+
+   ```env
+   DB_USER = your_mongodb_user
+   DB_PASS = your_mongodb_password
+   ACCESS_SECRET_TOKEN = your_jwt_access_token
+   ```
+
+   Replace `your_mongodb_user` , `your_mongodb_password` and `your_jwt_access_token` with your actual value.
+
+4. **Start the server**:
+
+   ```sh
+   npm start
+   ```
+
+   The server should now be running and accessible at `http://localhost:your_port_number`.
+
+## Scripts
+
+- `start`: Starts the server using `node index.js`.
+
+## Dependencies
+
+- `cors`: ^2.8.5
+- `dotenv`: ^16.4.5
+- `express`: ^4.19.2
+- `mongodb`: ^6.5.0
+
+## Author
+
+- Hasan Al Mahmud
---
+++ index.js
@@ -0,0 +1,176 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://admin:admin@cluster0.zgdxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const spotCollection = client.db("spotDB").collection("spots");
+    const countriesCollection = client
+      .db("countriesDB")
+      .collection("countries");
+
+    //countries related apis
+    app.get("/countries", async (req, res) => {
+      const result = await countriesCollection.find().toArray();
+      res.send(result);
+    });
+    app.get('/country/:countryId', async(req, res)=>{
+      const countryId = req.params.countryId;
+      // console.log(countryId);
+      const query = {_id: countryId};
+      const result = await countriesCollection.findOne(query);
+      res.send(result);
+     
+    })
+
+    //this api for set the multiple data to a countries db
+    app.post("/countries/data", async (req, res) => {
+      const data = req.body;
+      // console.log(data);
+      const options = { ordered: true };
+      // Execute insert operation
+      const result = await countriesCollection.insertMany(data, options);
+
+      res.send(result);
+    });
+    app.get('/countries/data', async(req, res)=>{
+      const result = await countriesCollection.find().toArray();
+      res.send(result);
+    })
+
+    // spotDB realted apis
+    app.post("/addSpot", async (req, res) => {
+      const spotInfo = req.body;
+      const result = await spotCollection.insertOne(spotInfo);
+      res.send(result);
+    });
+    app.get("/allSpot", async (req, res) => {
+      const cursor = spotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/allSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await spotCollection.findOne(query);
+
+      res.send(result);
+    });
+    // sorting on average cost ascending and descending order
+    app.get("/allSpot/sortOrder/:orderId", async (req, res) => {
+      const id = req.params.orderId;
+      const cursor = spotCollection.find();
+      if (id == 1) {
+        const result = await cursor.sort({ averageCost: 1 }).toArray();
+        res.send(result);
+      } else if (id == 2) {
+        const result = await cursor.sort({ averageCost: -1 }).toArray();
+        res.send(result);
+      } else {
+        const result = await cursor.toArray();
+        res.send(result);
+      }
+    });
+
+    //get data filter by email
+    app.get("/mylist/:email", async (req, res) => {
+      const email = req.params.email;
+      // console.log(email);
+      const query = { email: email };
+      const cursor = spotCollection.find(query);
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+    app.get("/countrySpot", async (req, res) => {
+      const pipeline = [
+        {
+          $group: {
+            _id: "$countryName",
+            spot: { $first: "$$ROOT" },
+          },
+        },
+        {
+          $replaceRoot: { newRoot: "$spot" },
+        },
+      ];
+      const cursor = spotCollection.aggregate(pipeline);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/allSpotCountry", async (req, res) => {
+      const pipeline = [
+        {
+          $group: {
+            _id: "$countryName",
+            data: { $push: "$$ROOT" },
+          },
+        },
+      ];
+      const cursor = spotCollection.aggregate(pipeline);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.patch("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const info = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateInfo = {
+        $set: {
+          ...info,
+        },
+      };
+      const result = await spotCollection.updateOne(query, updateInfo);
+      res.send(result);
+    });
+
+    app.delete("/deleteSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await spotCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("nova tourism server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port:${port}`);
+});
